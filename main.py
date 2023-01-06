Fixed causal problem by transposing before head projecting matrix

main.py
@@ -12,7 +12,7 @@ sequence_length = 8
 N = 1_000_000
 loss = nn.CrossEntropyLoss()
 
-force_train = False
+force_train = True
 
 # Generate dataset of N sequences of heads/tails with uniform probability of length L
 def generate_dataset(N, L):
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     total_data = generate_dataset(N, sequence_length).to(device)
     # We need to mask out the future tokens
     mask = torch.tril(torch.ones(sequence_length-1, sequence_length-1)).to(torch.int64).to(device)
+    
 
     # input has shape (N, L)
     # output has shape (N, L, 2)

test.py
@@ -12,7 +12,7 @@ X = torch.rand((B, N, D))
 mask = torch.tril(torch.ones(X.shape[1], X.shape[1])).to(torch.int64)
 Z = torch.zeros((B, N, N))
 Z.masked_fill_(mask == 0, float('-inf'))
-print(Z)
+print(torch.softmax(Z, dim=2))
 
 
 layer = transformer.AttentionLayer(D, D, H)

transformer.py
@@ -28,8 +28,8 @@ class AttentionLayer(nn.Module):
         # We can do this by setting the upper triangle to -inf on the last two dimensions of QK^T
         if mask is not None:
             Q_KT.masked_fill_(mask == 0, float("-inf"))
-        # Now we can compute the softmax on the q dimension (rows)
-        Q_KT = torch.softmax(Q_KT, dim=2)
+        # Now we can compute the softmax on the k dimension (rows)
+        Q_KT = torch.softmax(Q_KT, dim=3)
         # Now we can compute the attention
         # In the unbatched case, QK^T = (q, k), V = (k, f)
         # Q^KT * V = (q, f)
@@ -38,7 +38,8 @@ class AttentionLayer(nn.Module):
         O = torch.einsum("bhqk, hff, bkf -> bhqf", Q_KT, self.W_V, X_V)
         # Now we can concatenate the heads
         # O = (b, h, q, f) -> O = (b, q, h*f)
-        O = O.reshape(O.shape[0], O.shape[2], O.shape[1] * O.shape[3])
+        O = O.transpose(1, 2)
+        O = O.reshape(O.shape[0], O.shape[1], O.shape[2] * O.shape[3])
         # Now we can project back to the original dimension
         return torch.einsum("bqx, xf -> bqf", O, self.O)
 